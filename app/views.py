7cc52d864c78d46d11f9ee36f3a621f958ad0f67
Diego Rivera <ederiverac@icloud.com>
2025-08-08 18:23:08 -0700

    djksandjsajd

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
