42dcb72889f61f60f4bdec34d598d162b9e7931b
Diego Rivera <ederiverac@icloud.com>
2025-10-09 08:06:43 -0700

    dsfafasdfasdfasfs

--- app/views.py
+++ app/views.py
@@ -2878,8 +2878,8 @@ def usuario_redirect_view(request):
             # Aquí podrías guardar qué avatar animado se seleccionó si tienes varios
             
         profile.save()
-        # Redirigir a la página de inicio usando el nombre de la URL
-        return redirect('home')  # Usar el nombre de la URL 'home' en lugar de la ruta completa
+        # Redirigir a la página de inicio usando la ruta completa con el prefijo /app/
+        return redirect('/app/home/')  # Usar la ruta completa con el prefijo /app/
         
     return render(request, 'personalizacion.html')
 
