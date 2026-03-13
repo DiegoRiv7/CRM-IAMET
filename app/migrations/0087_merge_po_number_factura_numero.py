22a7924815f6f9db5418e4ffbfc6241f43c2f47b
djrc312-ops <djrc312@gmail.com>
2026-03-13 16:02:27 -0700

    add merge migration for po_number and factura_numero

---
+++ app/migrations/0087_merge_po_number_factura_numero.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0086_todoitem_factura_numero'),
+        ('app', '0086_todoitem_po_number'),
+    ]
+
+    operations = [
+    ]
